9a659fd2707a8763c36cd68348ccce511c7e4f98
TEVolkov <78999784+TEVolkov@users.noreply.github.com>
2021-03-05 18:56:12 +0300

    feat(lab04): add directories

---
+++ lab04/prog_1.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import math
+
+w = 12.0
+g = 0.0
+
+def f(t):
+    f = 0
+    return f
+
+def dx(x, t):
+    dx1 = x[1]
+    dx2 = - w*x[0] - g*x[1] - f(t)
+    return dx1, dx2
+
+x0 = np.array([0.5, 1])
+
+t = np.arange(0, 51, 0.05)
+
+x = odeint(dx, x0, t)
+
+y1 = x[:, 0]
+y2 = x[:, 1]
+
+plt.plot(y1, y2)
+plt.grid('axis = "both"')
